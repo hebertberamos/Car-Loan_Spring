46d3992527bd4193025af4f1482e5c79096e1c10
Hebert Benigno <hebertramos09@gmail.com>
2023-12-27 20:30:31 -0300

    Initializing the project

---
+++ backend/src/main/java/com/personalproject/carloan/CarloanApplication.java
@@ -0,0 +1,13 @@
+package com.personalproject.carloan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarloanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarloanApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/personalproject/carloan/CarloanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personalproject.carloan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarloanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
