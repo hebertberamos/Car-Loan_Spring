6e1d2fc74952583e6d54b6f4421ae2a65de6cc4e
Hebert Benigno <hebertramos09@gmail.com>
2023-12-29 11:29:45 -0300

    Finished Client CRUD

--- backend/src/main/java/com/personalproject/carloan/controllers/ClientController.java
+++ backend/src/main/java/com/personalproject/carloan/controllers/ClientController.java
@@ -4,11 +4,10 @@ import com.personalproject.carloan.dtos.ClientDTO;
 import com.personalproject.carloan.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -29,5 +28,27 @@ public class ClientController {
     public ResponseEntity<ClientDTO> findById(@PathVariable Long id){
         ClientDTO dto = service.findById(id);
         return ResponseEntity.ok().body(dto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> update(@PathVariable Long id, @RequestBody ClientDTO clientDTO){
+        clientDTO = service.update(id, clientDTO);
+        return ResponseEntity.ok().body(clientDTO);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<String> delete(@PathVariable Long id){
+        ClientDTO clientDTO = service.delete(id);
+        if(clientDTO != null){
+            return  ResponseEntity.ok("Elemento com id " + id + " deletado com sucesso!");
+        }
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO clientDTO){
+        clientDTO = service.insert(clientDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(clientDTO.getId()).toUri();
+        return ResponseEntity.created(uri).body(clientDTO);
     }
 }
