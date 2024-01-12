fc08ab106b9a55ddd80cd422276ce0aea2d928de
Hebert Benigno <hebertramos09@gmail.com>
2024-01-12 19:25:00 -0300

    Custom validation response

---
+++ backend/src/main/java/com/personalproject/carloan/controllers/exceptions/ValidationError.java
@@ -0,0 +1,16 @@
+package com.personalproject.carloan.controllers.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError{
+    private List<FieldMessage> erros = new ArrayList<>();
+
+    public List<FieldMessage> getErros() {
+        return erros;
+    }
+
+    public void addNewError(String fieldName, String message){
+        erros.add(new FieldMessage(fieldName, message));
+    }
+}
